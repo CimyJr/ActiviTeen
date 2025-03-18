0bede93f5924067bd4c95a5bbd584cd727a01181
CimyJr <juniorqueiros45@gmail.com>
2025-03-18 15:11:23 -0300

    Initial Commit

---
+++ ActivTeen/ActivTeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivTeenApp.swift
+//  ActivTeen
+//
+//  Created by user on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ActivTeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
