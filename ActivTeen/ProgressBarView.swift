994df9c607a9ae355a0a358c09382dd55231aaf8
Tainara <anatainarabat@gmail.com>
2025-03-20 17:52:12 -0300

    refact: I added the base code for the progress bar

---
+++ ActivTeen/ProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarView.swift
+//  ActivTeen
+//
+//  Created by found on 20/03/25.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBarView()
+}
