4dc5d03c4be4f65e91662cfe8e7085c6bbb20968
Tainara <anatainarabat@gmail.com>
2025-03-26 17:00:19 -0300

    refactor(UI): visual and architectural updates
    1. DESIGN:
    - Updated color system (new primary palette)
    - Refreshed all illustration assets
    
    2. ARCHITECTURE:
    - Removed CombineView (@deprecated)
    - Added ReadingActivitiesView (feature-complete)
    
    3. CHORES:
    - Deleted unused image files (12)
    - Updated UI tests

---
+++ ActivTeen/MusicActivity/BooksView.swift
@@ -0,0 +1,18 @@
+//
+//  BooksView.swift
+//  ActivTeen
+//
+//  Created by found on 26/03/25.
+//
+
+import SwiftUI
+
+struct BooksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BooksView()
+}
